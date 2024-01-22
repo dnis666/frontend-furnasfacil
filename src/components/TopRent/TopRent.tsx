4b5e3c9bad473ed63925a2bcbcdb7795afb2a5a2
Gustavo Teixeira <gustavotc95@gmail.com>
2024-01-22 15:33:02 -0300

    Ajustando tag disponivel

--- src/components/TopRent/TopRent.tsx
+++ src/components/TopRent/TopRent.tsx
@@ -1,6 +1,6 @@
-import TopImg from "../../assets/top-house.png";
+import TopImg from '../../assets/top-house.png';
 import { useDevice } from '../DeviceContext/DeviceContext';
-import "./TopRent.css";
+import './TopRent.css';
 
 const TopRent = () => {
   const { isMobile } = useDevice();
@@ -9,10 +9,10 @@ const TopRent = () => {
 
   const divStyle = {
     backgroundImage: `url(${topRentImg})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    width: "1207px",
-    height: "258px",
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '1207px',
+    height: '258px',
   };
 
   return isMobile ? (<></>) : (
@@ -22,9 +22,11 @@ const TopRent = () => {
           <div className="toprent-house" style={divStyle}>
             <div className="toprent-info">
               <div className="ring-container">
-                
-                <div className="ringring"></div>
-                <div className="circle"><strong>DISPONÍVEL</strong></div>
+                <div className="circle-container">                
+                  <div className="ringring"></div>
+                  <div className="circle"></div>
+                </div>
+                <div className="text"><strong>DISPONÍVEL</strong></div>
               </div>
             </div>
           </div>
